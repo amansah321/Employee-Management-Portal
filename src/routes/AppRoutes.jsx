d04ef50ec6379644a21e172558d9d643945a38b7
Aman Kumar <friendaman76@gmail.com>
2026-07-12 16:49:47 +0530

    feat:add not found page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.jsx";
+import NotFound from "../pages/NotFound.jsx";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
