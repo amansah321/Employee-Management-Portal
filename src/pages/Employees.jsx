d959ae5d3d9ffd9d1ecffa068d892988d8db8b9d
Aman Kumar <friendaman76@gmail.com>
2026-07-13 19:09:40 +0530

    Set up Employees state for API integration

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Employees() {
+  const [employees, setEmployees] = useState([]);
   return (
     <div>
       <h1>Employees</h1>
