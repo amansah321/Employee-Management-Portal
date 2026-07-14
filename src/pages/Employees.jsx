e34ba14ab10221fb609ed02fd7ee57dc465641b9
Aman Kumar <friendaman76@gmail.com>
2026-07-14 18:51:42 +0530

    Fetch and display employees using DummyJSON API

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -4,10 +4,27 @@ import axios from "axios";
 
 function Employees() {
   const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/users")
+      .then((response) => {
+        setEmployees(response.data.users);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(employees);
   return (
     <div>
       <h1>Employees</h1>
       <p>Welcome to Employees Page</p>
+
+      {employees.map((employee) => {
+        return <h2 key={employee.id}>{employee.firstName}</h2>;
+      })}
     </div>
   );
 }
